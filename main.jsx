de004c567c2aa9201609b0d0df421b3bf03bb4e4
asyylz <49663592+asyylz@users.noreply.github.com>
2024-03-07 12:21:16 +0000

    deploy update

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./src/App";
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
-import "./App.css";
+import "./src/index.css";
+import "./src/App.css";
 
 ReactDOM.createRoot(document.querySelector(".container")).render(
   <React.StrictMode>
